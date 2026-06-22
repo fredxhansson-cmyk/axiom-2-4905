481f6b1e46bec6f98be21bc2a4c62237379994ac
fredxhansson-cmyk <fredxhansson@gmail.com>
2026-06-22 20:28:11 +0200

    add pages/success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Tack för ditt köp!</h1>
+      <p>Vi hoppas du kommer att älska dina nya plagg.</p>
+    </div>
+  );
+}
